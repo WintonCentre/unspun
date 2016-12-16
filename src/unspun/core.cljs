9fcd0da3fa523716b2c97e465b10f5a3317168c5
gmp26 <gmp26@cam.ac.uk>
2016-12-16 22:26:27 +0000

    added more palettes

--- src/unspun/core.cljs
+++ src/unspun/core.cljs
@@ -3,14 +3,16 @@
   (:require [re-natal.support :as support]
             [rum.core :as rum]
             [cljs-exponent.components :refer [text view image touchable-highlight] :as rn]
-            [unspun.shared.palette :refer [material-blue-orange]]))
+            [unspun.shared.palettes :refer [palettes]]))
 
 
 ;;;
 ;; design globals
 ;;;
 (def logo-img (js/require "./assets/images/logo.png"))
-(def app-palette material-blue-orange)
+
+#_(defn palette [] (:deep-purple-pink palettes))
+(defn palette [] (:cyan-deep-orange palettes))
 
 
 (defn alert [title]
@@ -18,35 +20,37 @@
 
 (defonce app-state (atom {:brand "Winton Centre"}))
 
-(def page-style {
-                 :marginLeft      0
-                 :padding         0
-                 :alignItems      "center"
-                 :justifyContent  "space-around"
-                 :backgroundColor (:primary app-palette)})
+(defn page-style []
+  {:marginLeft      0
+   :padding         0
+   :alignItems      "center"
+   :justifyContent  "space-around"
+   :backgroundColor (:primary (palette))})
 
-(def brand-style {:fontSize   30
-                  :fontWeight "400"
-                  :textAlign  "center"
-                  :color      (:dark-primary app-palette)})
+(defn brand-style []
+  {:fontSize   30
+   :fontWeight "400"
+   :textAlign  "center"
+   :color      (:light-primary (palette))})
 
+(def shadow-size)
 
 (defc AppRoot < rum/reactive [state]
-  (view {:fill 1 :style page-style}
-        (text {:style (merge {:paddingTop 40} brand-style)}
+  (view {:fill 1 :style (page-style)}
+        (text {:style (merge {:paddingTop 40} (brand-style))}
               (:brand @state))
         (image {:source logo-img
                 :style  {:transform [{:scale 0.5}]}})
         (touchable-highlight {:style   {:margin          40
-                                        :backgroundColor (:accent app-palette)
+                                        :backgroundColor (:accent (palette))
                                         :borderRadius    30
                                         :height          60
                                         :width           60
                                         :shadowColor     "#000"
-                                        :shadowOffset    {:width 3 :height 3}
-                                        :shadowRadius 3
-                                        :shadowOpacity 0.5
-                                        :alignItems "center"
+                                        :shadowOffset    {:width 2 :height 2}
+                                        :shadowRadius    2
+                                        :shadowOpacity   0.5
+                                        :alignItems      "center"
                                         :justifyContent  "center"
                                         }
                               :onPress #(alert "HELLO!")}
---
+++ src/unspun/shared/palettes.cljs
@@ -0,0 +1,45 @@
+(ns unspun.shared.palettes)
+
+;;;
+; See https://www.materialpalette.com/blue/orange
+;;;
+(def palettes
+  {:blue-orange
+   {:dark-primary   "#1875D1"
+    :primary        "#2095F2"
+    :light-primary  "#BBDEFB"
+    :text-icons     "#FEFEFE"
+    :accent         "#FE9700"
+    :primary-text   "#212121"
+    :secondary-text "#757575"
+    :divider        "#BDBDBD"}
+
+   :cyan-deep-orange
+   {:dark-primary   "#0097A7"
+    :primary        "#00BCD4"
+    :light-primary  "#B2EBF2"
+    :text-icons     "#FEFEFE"
+    :accent         "#FF5722"
+    :primary-text   "#212121"
+    :secondary-text "#757575"
+    :divider        "#BDBDBD"}
+
+   :light-green-teal
+   {:dark-primary   "#689F38"
+    :primary        "#8BC34A"
+    :light-primary  "#DCEDC8"
+    :text-icons     "#FEFEFE"
+    :accent         "#009688"
+    :primary-text   "#212121"
+    :secondary-text "#757575"
+    :divider        "#BDBDBD"}
+
+   :deep-purple-pink
+   {:dark-primary   "#512DA8"
+    :primary        "#673AB7"
+    :light-primary  "#D1C4E9"
+    :text-icons     "#FEFEFE"
+    :accent         "#FF4081"
+    :primary-text   "#212121"
+    :secondary-text "#757575"
+    :divider        "#BDBDBD"}})
