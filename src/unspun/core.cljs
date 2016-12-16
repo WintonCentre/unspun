96f74409d279cd5a0f8dd5774d8086dcc8c17346
gmp26 <gmp26@cam.ac.uk>
2016-12-16 14:02:01 +0000

    Added material palette and icons

--- src/unspun/core.cljs
+++ src/unspun/core.cljs
@@ -1,21 +1,37 @@
 (ns unspun.core
-    (:require-macros [rum.core :refer [defc]])
-    (:require [re-natal.support :as support]
-              [rum.core :as rum]
-              [cljs-exponent.components :refer [text view image touchable-highlight] :as rn]))
+  (:require-macros [rum.core :refer [defc]])
+  (:require [re-natal.support :as support]
+            [rum.core :as rum]
+            [cljs-exponent.components :refer [text view image touchable-highlight] :as rn]
+            [unspun.shared.palette :refer [material-blue-orange]]))
 
-(def logo-img (js/require "./assets/images/cljs.png"))
+
+;;;
+;; design globals
+;;;
+(def logo-img (js/require "./assets/images/logo.png"))
+(def app-palette material-blue-orange)
+
 
 (defn alert [title]
   (.alert rn/alert title))
 
 (defonce app-state (atom {:greeting "Hello Clojure in iOS and Android!"}))
 
+
+
+(def page-style {:flexDirection   "column"
+                 :flex            1
+                 :marginLeft      0
+                 :paddingLeft 10
+                 :alignItems      "flex-start"
+                 :backgroundColor (:primary app-palette)})
+
+
 (defc AppRoot < rum/reactive [state]
-  (view {:style {:flexDirection "column" :margin 40 :alignItems "center"}}
-        (text {:style {:fontSize 30 :fontWeight "100" :marginBottom 20 :textAlign "center"}} (:greeting @state))
-        (image {:source logo-img
-                :style  {:width 80 :height 80 :marginBottom 30}})
+  (view {:style page-style}
+        (text {:style {:fontSize 30 :fontWeight "100" :marginBottom 20 :textAlign "center"}} (str @state))
+        (image {:source logo-img})
         (touchable-highlight {:style   {:backgroundColor "#999" :padding 10 :borderRadius 5}
                               :onPress #(alert "HELLO!")}
                              (text {:style {:color "white" :textAlign "center" :fontWeight "bold"}} "press me"))))
---
+++ src/unspun/shared/palette.cljs
@@ -0,0 +1,15 @@
+(ns unspun.shared.palette)
+
+;;;
+; See https://www.materialpalette.com/blue/orange
+;;;
+(def material-blue-orange
+  {
+   :dark-primary   "#1875D1"
+   :primary        "#2095F2"
+   :light-primary  "#BBDEFB"
+   :text-icons     "#FEFEFE"
+   :accent         "#FE9700"
+   :primary-text   "#212121"
+   :secondary-text "#757575"
+   :divider        "#BDBDBD"})
